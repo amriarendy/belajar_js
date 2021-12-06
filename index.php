81ae3021393ba5de51eb67482fe33894f11de3e7
amriarendy <amriarendy90@gmail.com>
2021-12-06 17:52:21 +0700

    penambahan array

--- index.php
+++ index.php
@@ -389,19 +389,47 @@
 
 		// Kode menambah array
 
-		let arrayKosong = [];
-		let arrayNama = ["Amria", "Rendy", "Desbintra"];
-
 		// Cara data di array akan disimpan dalam posisi berurutan, dimana urutan pertama dimulai dari nomor 0.
 		// Setiap kita menambah data ke Array, otomatis data akan disimpan di urutan terakhir.
 		// Urutan di array, kita sebut dengan index.
 		// Perintah untuk
 
+		let arrayKosong = [];
+		let arrayNama = ["Amria", "Rendy", "Desbintra"];
+
 		const namesArray = [];
 		namesArray.push("Amria");
 		namesArray.push("Rendy", "Desbintra");
+		namesArray.push("Fauzan", "Meri", "Ade");
 
+		console.info("Ini tipe data array dasar");
 		console.table(namesArray);
+
+		// Operasi di Array
+		// array.push(value) => Menambah data ke Array
+		// array.length => Untuk mendapatkan panjang Array
+		// array[index] => Mendapat data di posisi index
+		// array[index] = value => Mengubah data di posisi index
+		// delete array[index] => Menghapus data di posisi index, namun index tidak bergeser
+
+		console.info(namesArray[0]);
+		console.info(namesArray[1]);
+		console.info(namesArray[2]);
+		console.info("Mengubah array value");
+
+		namesArray[0] = "Goican";
+		namesArray[1] = "id";
+		namesArray[2] = "Created Service";
+		console.table(namesArray);
+		console.info("Mengubah array value");
+
+		delete namesArray[2];
+		console.table(namesArray);
+		console.info("Menghapus array value");
+
+		namesArray.push("Ini pengganti yang dihapus array index[2]");
+		console.table(namesArray);
+		console.info("Mengganti index array value");
 	</script>
 </body>
 </html>
